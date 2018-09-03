af52f7560a8547b48fadd60e9238434f384df127
denasiahamilton <30352255+denasiahamilton@users.noreply.github.com>
2018-09-02 21:55:13 -0500

    Update game.py

--- Labs/game.py
+++ Labs/game.py
@@ -10,9 +10,29 @@ def main():
     display = pygame.display.set_mode((width, height))
     speed = 60
     time = pygame.time.Clock()
-    caption = ygame.display.set_caption(str(game.py))
+    caption = pygame.display.set_caption("game.py")
     display.fill((0,0,0))
-    image = pygame.image.load("imSquare")
+    image = pygame.image.load("imSquare.png")
+    imageRect = image.get_rect()
+    moving = [2,2]
+    paddle = pygame.image.load("ImRectangle")
+    imagePaddle = paddle.get_rect()
+    
+   while True:
+    #game
+    imageRect = imageRect.move(moving)
+    display.blit(image, imageRect)
+    
+    pygame.display.flip()
+    time.tick(speed)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+            
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                display.fill((random.randint(0,255), random.randint(0,255), random.randint(0,255)))
     
 
 if __name__ == "__main__":
